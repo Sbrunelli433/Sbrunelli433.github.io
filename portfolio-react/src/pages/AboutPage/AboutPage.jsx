8b4dec14fc65dab2aed2afe64b2b46e2d52f6091
Steve Brunelli <brunes46@gmail.com>
2021-06-08 09:42:52 -0500

    content popped about and contact pages, updated styles on header. updated padding styles on app.css to give it a better look

--- portfolio-react/src/pages/AboutPage/AboutPage.jsx
+++ portfolio-react/src/pages/AboutPage/AboutPage.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './AboutPage.styles.scss';
+
 const AboutPage = () => {
 
     return (
-        <div>
+        <div className="about-page">
             <h1>This is my About Page</h1>
+            <div className="top-content">
+                <p>My Name is Steve Brunelli</p>
+                <p>I am a devCodeCamp Alumnus</p>
+                <p>I am a Front-End Web Developer</p>
+                <p>Milwaukee is my home</p>
+                <p>Driven by Growth and Passion</p>
+            </div>
+            <div className="bottom-content">
+                <p>I am a Milwaukee Native</p>
+                <p>I am a fan of the Milwaukee Bucks</p>
+                <p>I coach part time for the Milwaukee Sting Volleyball Club</p>
+            </div>
         </div>
     );
 }
--- portfolio-react/src/pages/ContactPage/ContactPage.jsx
+++ portfolio-react/src/pages/ContactPage/ContactPage.jsx
@@ -6,6 +6,10 @@ const ContactPage = () => {
     return (
         <div>
             <h1>This is my Contact Page</h1>
+            <a href="https://github.com/Sbrunelli433">Github</a>
+            <a href="https://www.linkedin.com/in/stevebrunelli/">LinkedIn</a>
+            <a href="https://twitter.com/SteveBrunelli?lang=en">Twitter</a>
+            <a href="mailto:brunes46@gmail.com">Gmail</a>
         </div>
     );
 }
